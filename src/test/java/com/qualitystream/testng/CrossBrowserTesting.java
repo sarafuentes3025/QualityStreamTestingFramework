4c03b1479a08ed2f8551850cd8a7024d7da63cc3
Sara Fuentes <sara.fuentes3025@gmail.com>
2021-08-30 21:07:18 -0500

    Add CrossBrowserTesting class

---
+++ src/test/java/com/qualitystream/testng/CrossBrowserTesting.java
@@ -0,0 +1,26 @@
+package com.qualitystream.testng;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class CrossBrowserTesting {
+
+  private WebDriver driver;
+
+  @BeforeClass
+  public void beforeClass(){
+
+  }
+
+  @Test
+  public void googleSearch(){
+
+  }
+
+  @AfterClass
+  public void afterClass(){
+
+  }
+}
