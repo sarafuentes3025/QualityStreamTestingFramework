c92917b99f4cd445a12ece5f13557cf288335e75
Sara Fuentes <sara.fuentes3025@gmail.com>
2021-09-01 14:52:11 -0500

    Add SearchBlouses class

---
+++ src/test/java/com/reportsExample/qs/SearchBlouses.java
@@ -0,0 +1,20 @@
+package com.reportsExample.qs;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class SearchBlouses {
+
+  @Test
+  public void f() {
+  }
+
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+}
