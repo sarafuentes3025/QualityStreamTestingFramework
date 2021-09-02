3722ee4d4806afea30a138bf303bde17b9c3376b
Sara Fuentes <sara.fuentes3025@gmail.com>
2021-09-02 16:35:54 -0500

    Add new package

---
+++ src/test/java/com/quality/stream/code/examples/Download_File.java
@@ -0,0 +1,21 @@
+package com.quality.stream.code.examples;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Download_File {
+
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @Test
+  public void testName() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+
+  }
+}
